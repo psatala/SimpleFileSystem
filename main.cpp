89f222b0fb59688d350413f0fc844dc20a74bdb2
psatala <psatala@mion.elka.pw.edu.pl>
2020-01-16 03:02:21 +0100

    Work on distribution of data within a disk

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <unistd.h>
 
+#define MAX_DISK_SIZE 128 * 1024 * 1024
 #define BLOCK_SIZE 4096
+#define AVERAGE_FILE_SIZE_IN_BLOCKS 2
+#define N_FILES_PER_I_NODE_BLOCK 32
+
 
 using namespace std;
 
@@ -56,6 +60,7 @@ void setVDiskSize(FILE* vDiskFile)
     cin >> newSize;
 
     newSize = newSize - newSize % BLOCK_SIZE; ///resize to be a multiply of block size
+    newSize = min(newSize, MAX_DISK_SIZE);    ///resize if size bigger than allowed
 
     fseek(vDiskFile, newSize - 1, SEEK_SET);
     fprintf(vDiskFile, "x");
@@ -71,18 +76,30 @@ int getVDiskSize(FILE* vDiskFile)
 void setVDiskParameters(FILE* vDiskFile)
 {
     ///based on disk size, calculate where bitmaps, i-node table and user data are
-    int vDiskSize = getVDiskSize(vDisk);
+    int vDiskSize = getVDiskSize(vDiskFile);
     int nBlocks = vDiskSize / BLOCK_SIZE;
-    int nInodeBlocks =
+    int freeBlocks = nBlocks - 2;
+    int nInodeBlocks = freeBlocks / (N_FILES_PER_I_NODE_BLOCK * AVERAGE_FILE_SIZE_IN_BLOCKS + 1);
     /** free blocks = nBlocks - 2
-        average file -> 1 block
+        average file -> 2 blocks
         x - number of files
-        each i-node:
+        each i-node: 128B
             nLinks = 8
-            nBlockAddresses = 256
+            nBlockAddresses = 31
             8B * nLinks = 64B
-            4B * nBlockAddresses = 1024B
-            1B isDirectory
+            2B * nBlockAddresses = 62B (short ints!!!)
+            2B other (e.g. isDirectory)
+
+            maxFileSize = 124kB
+
+            each i-node block -> 32 files
+            32 * nInodeBlocks ~= (freeBlocks - nInodeBlocks) / averageFileSizeInBlocks
+            64 nIB = f - nIB
+            65 nIB = f
+            nIB = f / 65
+
+            maxDiskSize = 128MB <- calculated from size of data bitmap (4kB = 32kb, which gives 32768 data blocks, each 4kB, total 128MB),
+            also limited by (unsigned) short int range (65536)
     **/
 
     int iNodeBitmapIndex = 0;
