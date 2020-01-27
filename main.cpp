853b3f1b7842823c1ad5c46ebe8e350b8041bdb7
psatala <psatala@mion.elka.pw.edu.pl>
2020-01-27 17:27:23 +0100

    Add one more test and delete unnecessary comments

--- main.cpp
+++ main.cpp
@@ -16,37 +16,6 @@ int main(int argc, char** argv)
         diskSize = atoi(argv[2]);
 
     CommandLineInterpreter myCMD(argv[1], diskSize);
-    /*VirtualDisk myDisk(argv[1], diskSize);
-    myDisk.createNewDirectory("newDir");
-    myDisk.listDirectory();
-    myDisk.changeDirectory("newDir");
-    myDisk.listDirectory();
-    myDisk.createNewDirectory("../wololo");
-    myDisk.changeDirectory("..");
-    myDisk.copyToVDisk("wololo/test2");
-    myDisk.listDirectory();
-    myDisk.changeDirectory("wololo");
-    myDisk.listDirectory();
-    myDisk.printDiskUsageInfo();
-    myDisk.changeDirectory("../newDir");
-    myDisk.addLink("../wololo/test2", "../linkToTest2");
-    myDisk.printPath();
-    myDisk.changeDirectory("..");
-    myDisk.listDirectory();
-    myDisk.changeDirectory("wololo");
-    myDisk.listDirectory();
-    myDisk.createNewDirectory("foobar");
-    myDisk.changeDirectory("foobar");
-    myDisk.printPath();*/
-    /*myDisk.copyToVDisk("test2");
-    myDisk.listDirectory(0);
-    myDisk.printDiskUsageInfo();
-    myDisk.deleteBytes("test2", 10000);
-    myDisk.printDiskUsageInfo();
-    myDisk.addBytes("test2", 12000);
-    myDisk.printDiskUsageInfo();
-    myDisk.copyFromVDisk("test2");
-    myDisk.listDirectory(0);
-*/
+
     return 0;
 }
